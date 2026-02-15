84448fbd07cfe2bc45cf3a6e88fea01bbab5ce4a
jbw9964 <jbw9964@gmail.com>
2026-02-15 15:30:16 +0900

    feedback(feat): 구입 금액으로 로또 수량을 제공하는 `LotterySeller` class 구성
    +feedback(refactor): 불필요한 exception 제거

--- src/main/java/lottery/exception/InsufficientPurchasePriceException.java
+++
@@ -1,8 +0,0 @@
-package lottery.exception;
-
-public class InsufficientPurchasePriceException extends RuntimeException {
-
-    public InsufficientPurchasePriceException(String message) {
-        super(message);
-    }
-}
